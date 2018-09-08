7e2bc0c1978c1cc40091bb17fba7e58fba4bf2d8
Bruno Fernandes <bruno.fernandesbfs@gmail.com>
2018-09-08 15:50:48 -0300

    Create system to stype parse

--- StyleSource/Core/Parses/StyleParser.swift
+++ StyleSource/Core/Parses/StyleParser.swift
@@ -9,16 +9,115 @@
 import PathKit
 import Yams
 
+public struct Style {
+    var key: String
+    var className: String
+    var elements: [Element]
+}
+
+public struct Element {
+    var key: String
+    var value: Any
+    var childs: [Element]
+
+    subscript(_ key: String) -> Element? {
+        return childs.filter { $0.key == key }.first
+    }
+
+    init(key: String, value: Any) {
+        self.key = key
+        self.value = value
+        self.childs = []
+    }
+
+    init(key: String, childs: [Element]) {
+        self.key = key
+        self.value = NSNull()
+        self.childs = childs
+    }
+}
+
 public func styleParse(path: Path) throws -> [StyleModel] {
 
     let data: String = try path.read()
 
-    guard let value = try Yams.load(yaml: data) as? Json,
-        let style = value[ConstantKeys.style] as? Json else {
-            throw Errors.yamlInvalid
+    guard var value = try Yams.load(yaml: data) as? Json else {
+        throw Errors.yamlInvalid
     }
 
-    return discoverRoots(style)
+    value = value.filter { (key, value) -> Bool in
+        guard let value = value as? Json else {
+            return false
+        }
+        return value.contains { $0.key == "class" }
+    }
+
+    var styles: [Style] = []
+
+    for (key, style) in value {
+
+        guard let style = style as? Json else {
+            continue
+        }
+
+        let isClass = style.contains { $0.key == "class" }
+
+        if isClass, let className = style["class"] as? String {
+
+            let data = style.filter { $0.key != "class" }
+
+            let elements = findElement(data: data)
+
+            let model = Style(key: key, className: className, elements: elements)
+            styles.append(model)
+        }
+    }
+
+    return []
+}
+
+public func findElement(data: Json) -> [Element] {
+
+    var elements: [Element] = []
+    for (key, value) in data {
+
+        if let value = value as? Json {
+            let childs = findElement(data: value)
+
+            elements.append(Element(key: key, childs: childs))
+        }
+        else {
+            elements.append(Element(key: key, value: value))
+        }
+    }
+
+    return mergeElements(data: elements)
+}
+
+public func mergeElements(data: [Element]) -> [Element] {
+
+    var elements: [Element] = []
+    for element in data {
+        if element.key == "style" {
+
+            let sameRoot =  element.childs.filter { elm in data.contains { elm.key == $0.key } }
+
+            for item in sameRoot {
+
+                if let value = element[item.key] {
+                    print(value.key)
+                }
+            }
+
+
+            elements.append(contentsOf: element.childs)
+        }
+        else {
+            elements.append(element)
+        }
+    }
+
+    return elements
 }
 
 public func discoverRoots(_ root: Json) -> [StyleModel] {
@@ -78,6 +177,6 @@ public class StyleParser {
             ConstantKeys.style: data
         ]
 
-        return context
+        return [:] //context
     }
 }
