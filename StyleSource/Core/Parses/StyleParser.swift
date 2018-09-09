b3f5db39a6e30dbc6eb33a465200709755be4580
Bruno Fernandes <bruno.fernandesbfs@gmail.com>
2018-09-08 21:17:34 -0300

    Fix parse Style model

--- StyleSource/Core/Parses/StyleParser.swift
+++ StyleSource/Core/Parses/StyleParser.swift
@@ -20,10 +20,6 @@ public struct Element {
     var value: Any
     var childs: [Element]
 
-    subscript(_ key: String) -> Element? {
-        return childs.filter { $0.key == key }.first
-    }
-
     init(key: String, value: Any) {
         self.key = key
         self.value = value
@@ -37,19 +33,12 @@ public struct Element {
     }
 }
 
-public func styleParse(path: Path) throws -> [StyleModel] {
+public func styleParse(path: Path) throws -> [Style] {
 
     let data: String = try path.read()
 
-    guard var value = try Yams.load(yaml: data) as? Json else {
+    guard let value = try Yams.load(yaml: data) as? Json else {
         throw Errors.yamlInvalid
-    }
-
-    value = value.filter { (key, value) -> Bool in
-        guard let value = value as? Json else {
-            return false
-        }
-        return value.contains { $0.key == "class" }
     }
 
     var styles: [Style] = []
@@ -73,7 +62,7 @@ public func styleParse(path: Path) throws -> [StyleModel] {
         }
     }
 
-    return []
+    return styles
 }
 
 public func findElement(data: Json) -> [Element] {
@@ -91,25 +80,14 @@ public func findElement(data: Json) -> [Element] {
         }
     }
 
-    return mergeElements(data: elements)
+    return removeLevelElements(data: elements)
 }
 
-public func mergeElements(data: [Element]) -> [Element] {
+public func removeLevelElements(data: [Element]) -> [Element] {
 
     var elements: [Element] = []
     for element in data {
         if element.key == "style" {
-
-            let sameRoot =  element.childs.filter { elm in data.contains { elm.key == $0.key } }
-
-            for item in sameRoot {
-
-                if let value = element[item.key] {
-                    print(value.key)
-                }
-            }
-
-
             elements.append(contentsOf: element.childs)
         }
         else {
@@ -118,53 +96,6 @@ public func mergeElements(data: [Element]) -> [Element] {
     }
 
     return elements
-}
-
-public func discoverRoots(_ root: Json) -> [StyleModel] {
-
-    var model: [StyleModel] = []
-
-    for (key, value) in root {
-
-        if let data = value as? Json {
-            let elements = discoverElement(data: data)
-            model.append(StyleModel(key: key,
-                                    className: elements.1,
-                                    elements: elements.0,
-                                    cases: (elements.0.map { $0.key })))
-        }
-    }
-
-    return model
-}
-
-public func discoverElement(data: Json) -> ([StyleElement], String) {
-
-    var elements: [StyleElement] = []
-    guard let className = data["class"] as? String else {
-        fatalError("missing class property")
-    }
-
-    for (key, value) in data {
-
-        if let item = value as? Json {
-            let values = discoverValues(item: item)
-            elements.append(StyleElement(key: key, styles: values))
-        }
-    }
-
-    return (elements, className)
-}
-
-public func discoverValues(item: Json) -> [StyleValue] {
-
-    var values: [StyleValue] = []
-
-    for (key, value) in item {
-        values.append(StyleValue(name: key, data: value))
-    }
-
-    return values
 }
 
 public class StyleParser {
@@ -177,6 +108,6 @@ public class StyleParser {
             ConstantKeys.style: data
         ]
 
-        return [:] //context
+        return context
     }
 }
